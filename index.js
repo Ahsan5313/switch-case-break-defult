d79c386299ae725270b3284bbd9ffc04878a6bb6
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-04-27 16:24:53 +0600

    add javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+let text;
+let rollNumber = 5;
+
+switch (rollNumber){
+
+    case 1:
+    text="Congratulation! you are first";
+    break;
+    case 2:
+        text = "you are second";
+        break;
+        case 3 :
+            text = "you are third";
+            break;
+            case 4:
+                text  = "you are four";
+                break;
+                default:
+                    text = "what is your roll?"
+}
+
+console.log(text);
